540424caa04fea11e02c9b0b79994ef65b3def2c
Cory Cox <corycox@Corys-MBP.lan>
2023-04-15 09:37:40 -0400

    css and adding data to db

--- mytool/models.py
+++ mytool/models.py
@@ -18,9 +18,10 @@ class Tool(models.Model):
 class Battery(models.Model):
     brand = models.CharField(default="", max_length=20, blank=False)
     voltage = models.IntegerField(default=1, blank=False)
-    amperage = models.IntegerField(default=0, blank=False)
+    amperage = models.DecimalField(
+        default=0, max_digits=2, decimal_places=1, blank=False)
     HIGH_PERFORMANCE = [("Y", "Yes"), ("N", "No")]
     performance_choice = models.CharField(default="",
-                                          max_length=1, choices=HIGH_PERFORMANCE)
+                                          max_length=1, help_text="Select yes if high performance battery.", choices=HIGH_PERFORMANCE)
     quantity = models.IntegerField(default=0, blank=False)
     description = models.CharField(default="", max_length=150)
